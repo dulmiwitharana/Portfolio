db5b81ab75ae0696d3537bb41965059cebed4c87
Dulmi Witharana <dulmiwitharana@gmail.com>
2024-07-13 18:11:58 +0530

    second commit

--- src/constants/index.js
+++ src/constants/index.js
@@ -3,6 +3,9 @@ import project2 from "../assets/assets/projects/project-2.jpeg";
 import project3 from "../assets/assets/projects/project-3.jpeg";
 import project4 from "../assets/assets/projects/project-4.jpeg";
 import project5 from "../assets/assets/projects/project-5.jpeg";
+import project6 from "../assets/assets/projects/food.jpeg";
+import project7 from "../assets/assets/projects/project-7.jpg"; // Ensure this path is correct
+
 
 export const HERO_CONTENT = `Aspiring and highly motivated IT undergraduate trainee pursuing a degree in Information Technology, with a strong foundation in web development, mobile app development, and database management. Adept at problem-solving, collaborating in team environments, and eager to apply theoretical knowledge to real-world challenges. Passionate about continuously learning new technologies and improving technical skills to contribute effectively to innovative projects. Seeking an opportunity to leverage academic knowledge and hands-on experience in a dynamic and growth-oriented organization.`;
 
@@ -46,6 +49,22 @@ export const PROJECTS = [
       "A personal portfolio website showcasing projects, skills, and contact information.",
     plink:"https://github.com/dulmiwitharana/Portfolio.git",
     technologies: ["HTML", "TailwindCSS", "React"],
+  },
+  {
+    title: "WordWise Dictionary Website",
+    image: project7,
+    description:
+      "Developed the WordWise dictionary website allowing users to find word meanings and examples, with a read-aloud feature for pronunciation assistance.",
+    plink:"https://github.com/dulmiwitharana/Dictionary-Web.git",
+    technologies: ["HTML", "CSS", "Java Script"],
+  },
+  {
+    title: "Taste Treasures Food Recipe Website",
+    image: project6,
+    description:
+      "Developed the TasteTreasures food recipe system enabling users to register, log in, add recipes and saving and viewing favorite recipes.",
+    plink:"https://github.com/dulmiwitharana/Food-Recipe-Website.git",
+    technologies: ["MongoDB", "Express.js", "Node.js", "React.js"],
   },
   {
     title: "SNAKE Game App",
